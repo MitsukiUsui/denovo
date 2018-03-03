dcd6ae7e0b8cf1a518549b60b9ea1cba0811c304
MitsukiUsui <mitsuo.com.mu@gmail.com>
2018-03-03 13:24:18 +0900

    ISSUE28: implemented phylogeny/format_tree.py

---
+++ phylogeny/format_tree.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+import pandas as pd
+from ete3 import PhyloTree
+
+sys.path.append("../helper")
+from myio import get_strain_lst, get_distance_mat
+
+def main(target, catalogFilepath, nwkFilepath, outFilepath):
+    catalogFilepath = "../data/{}/catalog.tsv".format(target)
+    catalog_df = pd.read_csv(catalogFilepath, sep="\t")
+    
+    nwkFilepath = "../data/{}/cluster.phb".format(target)
+    tree = PhyloTree(nwkFilepath)
+    
+    #rooting
+    outgroup_lst = list(catalog_df[catalog_df["outgroup"] == 1]["genome_id"])
+    ancestor = tree.get_common_ancestor(outgroup_lst)
+    tree.set_outgroup(ancestor)
+    
+    #pruning
+    represent_lst = list(catalog_df[catalog_df["represent"] == 1]["genome_id"])
+    tree.prune(outgroup_lst + represent_lst)
+    
+    #output
+    tree.write(outfile=outFilepath)
+    print("DONE: output {}".format(outFilepath))
+
+if __name__ == "__main__":
+    target = sys.argv[1]
+    catalogFilepath = "../data/{}/catalog.tsv".format(target)
+    nwkFilepath = "../data/{}/cluster.phb".format(target)
+    outFilepath = "../data/{}/cluster.format.phb".format(target)
+    main(target, catalogFilepath, nwkFilepath, outFilepath)
+
--- phylogeny/pipeline.sh
+++ phylogeny/pipeline.sh
@@ -4,5 +4,6 @@ target=${1}
 statusFilename=${2:-.STATUS}
 
 ./myphylophlan.sh ${target}
-./represent.py ${target} 0.5
+./represent.py ${target} 0.05
+./format_tree.py ${target}
 echo 0 > ${statusFilename}
